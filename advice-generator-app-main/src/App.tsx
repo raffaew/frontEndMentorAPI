1895db3b2a600bf088ed0c601e4983d403cce769
Rafael Moreira Dias <rafael.moreira@afixcode.com.br>
2025-08-19 18:45:20 -0300

    basename

--- advice-generator-app-main/src/App.tsx
+++ advice-generator-app-main/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/advice-generator-app-main">
+    <BrowserRouter basename="/frontEndMentorAPI">
       <Home />
     </BrowserRouter>
   );
